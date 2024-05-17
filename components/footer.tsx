416e37114ffcf8886fd1d5b95e352e88c4680ff3
anshsingh7755 <121092055+anshsingh7755@users.noreply.github.com>
2024-05-17 20:02:40 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,9 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
-      <small className="mb-2 block text-xs text-gray-100">
-        
-      </small>
+      
       <p className="text-xs text-gray-400">
         <span className="font-semibold text-gray-300">About this website:</span>{" "}
         built with React & Next.js (App Router & Server Actions), TypeScript,
